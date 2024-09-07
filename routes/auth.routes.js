2190fdf194fa5b7ed6c8fcbcc6a5a13dd0baa7b6
Rohit Kumar <you@example.com>
2024-09-07 10:29:49 +0530

    1- auth controllers added

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,13 +1,13 @@
 import express from "express";
-import { signup, login, logout } from "../controllers/auth.controller.js";
+import { login, logout, signup, getMe } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
+// Define routes
+router.get("/me", protectRoute, getMe);
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
 
-
-
 export default router;
-
--- server.js
+++ server.js
@@ -1,19 +1,32 @@
 import express from "express";
 import dotenv from "dotenv";
-import connectMongoDB from "../backend/db/connectMongoDB.js"
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
+import connectMongoDB from "../backend/db/connectMongoDB.js"
+
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Middleware to parse request bodies
+app.use(express.json()); // Parses JSON req.body 
+app.use(express.urlencoded({ extended: true })); // Parses URL-encoded data
+app.use(cookieParser());
+
+// Routes
 app.use("/api/auth", authRoutes);
 
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-    connectMongoDB();
+// Start the server after connecting to the database
+connectMongoDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+}).catch(err => {
+    console.error('Failed to connect to MongoDB', err);
+    process.exit(1); // Exit process if DB connection fails
 });
 
